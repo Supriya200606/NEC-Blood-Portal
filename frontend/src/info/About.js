7911e7124b45e17f11aac5f9bc94b881c6df66ae
sujan-mishra001 <sujan48mishra@gmail.com>
2025-02-04 12:35:44 +0545

    23 th

--- frontend/src/info/About.js
+++ frontend/src/info/About.js
@@ -42,25 +42,25 @@ const About = () => {
           </section>
 
           <div className="flex flex-wrap justify-evenly items-center gap-6">
-            <div className="p-4 rounded-3xl bg-gradient-to-tr from-stone-400 via-grey-200 to-red-500 mb-10">
+            <div className="p-4 rounded-3xl bg-gradient-to-tr from-stone-400 via-grey-200 to-red-500 mb-10 w-full sm:w-auto">
               <div className="pb-6 p-6 text-3xl font-bold text-[#ff2323] bg-gray-100 border-4 rounded-3xl">
                 Verified Donors
                 <p className="flex text-justify justify-center">19</p>
               </div>
             </div>
-            <div className="p-4 rounded-3xl bg-gradient-to-tr from-stone-400 via-grey-200 to-red-500 mb-10">
+            <div className="p-4 rounded-3xl bg-gradient-to-tr from-stone-400 via-grey-200 to-red-500 mb-10 w-full sm:w-auto">
               <div className="p-6 text-3xl font-bold text-[#ff2323] bg-gray-100 border-4 rounded-3xl">
                 Active Donors
                 <p className="flex text-justify justify-center">10</p>
               </div>
             </div>
-            <div className="p-4 rounded-3xl bg-gradient-to-tr from-stone-400 via-grey-200 to-red-500 mb-10">
+            <div className="p-4 rounded-3xl bg-gradient-to-tr from-stone-400 via-grey-200 to-red-500 mb-10 w-full sm:w-auto">
               <div className="pb-6 p-6 text-3xl font-bold text-[#ff2323] bg-gray-100 border-4 rounded-3xl">
                 Total Donations
                 <p className="flex text-justify justify-center">34</p>
               </div>
             </div>
-            <div className="p-4 rounded-3xl bg-gradient-to-tr from-stone-400 via-grey-200 to-red-500 mb-10">
+            <div className="p-4 rounded-3xl bg-gradient-to-tr from-stone-400 via-grey-200 to-red-500 mb-10 w-full sm:w-auto">
               <div className="p-6 text-3xl font-bold text-[#ff2323] bg-gray-100 border-4 rounded-3xl">
                 Lives Saved
                 <p className="flex text-justify justify-center">--</p>
@@ -102,7 +102,7 @@ const About = () => {
           </div>
         </div>
         <div className="flex flex-wrap justify-around items-center h-auto">
-          <div className="flex flex-col items-center p-6 w-1/2 lg:w-1/4">
+          <div className="flex flex-col items-center p-6 w-full sm:w-1/2 lg:w-1/4">
             <div className="border-4 w-1/2 rounded-full mb-4">
               <img src={sujan} alt="logo" className="rounded-full" />
             </div>
@@ -118,7 +118,7 @@ const About = () => {
             </p>
           </div>
 
-          <div className="flex flex-col items-center p-6 w-1/2 lg:w-1/4">
+          <div className="flex flex-col items-center p-6 w-full sm:w-1/2 lg:w-1/4">
             <div className="border-4 w-1/2 rounded-full mb-4">
               <img src={ankit} alt="logo" className="rounded-full" />
             </div>
@@ -127,8 +127,8 @@ const About = () => {
               Backend
             </div>
             <p className="font-serif m-6 text-md">
-             A proud of Shreeyantra College who has successfully proven his work and intelligence in jjis 4 year career.
-             He is very friendly toward his every friends and veryresponsible toward his duty.
+              A proud of Shreeyantra College who has successfully proven his work and intelligence in his 4 year career.
+              He is very friendly toward his every friends and very responsible toward his duty.
             </p>
           </div>
         </div>
